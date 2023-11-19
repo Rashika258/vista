8015fd001258ccd6bb7fe1128ced7b3a7beff6cc
Rashika258 <rashikasuresh5758@gmail.com>
2023-11-19 17:40:34 +0530

    Ui component changes

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+"use client";
+
+import { sidebarLinks } from '@/constants';
+import { useAuth } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/router';
+
 
 const LeftSidebar = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+  const {userId} = useAuth()
   return (
-    <div>
-      
-    </div>
+    <section className='custom__scrollbar left__side__bar'>
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+
+          return(
+            <Link href={link.route} key={link.label} >
+              <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+              <p>{link.label}</p>
+            </Link>
+          )
+        })}
+
+    </section>
   );
 }
 
--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -1,11 +1,35 @@
 
+"use client";
+
+import { useRouter } from 'next/router';
 import React from 'react';
 
-const Pagination = () => {
+const Pagination = ({pageNumber, isNext, path}: {
+  pageNumber:number, isNext: boolean, path:string}) => {
+
+    const router = useRouter();
+
+
+    const handleNavigation = (type: string) => {
+      let nextPageNumber = pageNumber;
+      if (type === "prev") {
+        nextPageNumber = Math.max(1, pageNumber - 1);
+      } else if (type === "next") {
+        nextPageNumber = pageNumber + 1;
+      }
+
+      if(nextPageNumber > 1) {
+        router.push(`/${path}?page=${nextPageNumber}`)
+      } else {
+        router.push(`/${path}`)
+      }
+    };
+
+    if(!isNext && pageNumber === 1) return null;
   return (
-    <div>
-      
-    </div>
+    <section className='pagination__wrapper'>
+      <Button
+    </section>
   );
 }
 
