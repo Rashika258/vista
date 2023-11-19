5c601aee47a4fa7d97caead03320aed0b1bd7633
Rashika258 <rashikasuresh5758@gmail.com>
2023-11-19 10:35:49 +0530

    actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,75 +1,224 @@
+import { FilterQuery, SortOrder } from "mongoose";
 import { connectToDataBase } from "../dbConnection";
 import Community, { ICommunity } from "../models/community.model";
+import Thread from "../models/thread.model";
 import User, { IUserDocument } from "../models/user.model";
 
 export async function fetchUser(userId: string): Promise<IUserDocument | null> {
-    try {
-        await connectToDataBase();
+  try {
+    await connectToDataBase();
 
-        const user = await User.findOne<IUserDocument>({ id: userId })
-            .populate({
-                path: "communities",
-                model: Community,
-            })
-            .exec();
+    const user = await User.findOne<IUserDocument>({ id: userId })
+      .populate({
+        path: "communities",
+        model: Community,
+      })
+      .exec();
 
-        if (!user) {
-            console.error(`User with ID ${userId} not found`);
-            return null;
-        }
-
-        return user;
-    } catch (error) {
-        console.error("Error fetching user:", error);
-        throw error;
+    if (!user) {
+      console.error(`User with ID ${userId} not found`);
+      return null;
     }
-}
 
+    return user;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    throw error;
+  }
+}
 
 export async function updateUser({
-    id,
-    bio,
-    name,
-    userName, // Use the consistent field name
-    image,
-    // path, // Assuming you use path later
+  id,
+  bio,
+  name,
+  userName, // Use the consistent field name
+  image, // path, // Assuming you use path later
 }: IUserDocument): Promise<void> {
-    try {
-        await connectToDataBase();
+  try {
+    await connectToDataBase();
 
-        await User.findOneAndUpdate(
-            { id },
-            {
-                userName: userName.toLowerCase(), // Ensure consistent use of field names
-                name,
-                bio,
-                image,
-                onboarded: true,
-            },
-            {
-                upsert: true,
-            }
-        );
+    await User.findOneAndUpdate(
+      { id },
+      {
+        userName: userName.toLowerCase(), // Ensure consistent use of field names
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      {
+        upsert: true,
+      }
+    );
 
-        // Uncomment the following if block if you have a revalidatePath function and want to call it based on a condition.
-        // if (path === "/profile/edit") {
-        //     revalidatePath(path);
-        // }
-    } catch (error) {
-
-        if (error instanceof Error) {
-            throw new Error(`Failed to create/update user: ${error.message}`);
-        }
-        
+    // Uncomment the following if block if you have a revalidatePath function and want to call it based on a condition.
+    // if (path === "/profile/edit") {
+    //     revalidatePath(path);
+    // }
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Failed to create/update user: ${error.message}`);
     }
+  }
 }
 
+export async function fetchUserPosts(
+  userId: string
+): Promise<IUserDocument | null> {
+  try {
+    await connectToDataBase();
 
-export async function fetchUserPosts(userId: String) {
-    try {
-        await connectToDataBase();
+    // Find the user with the given userId
+    const user = await User.findOne({ id: userId });
 
-    }catch(error) {
-
+    if (!user) {
+      console.error(`User with ID ${userId} not found`);
+      return null; // or handle this case as needed
     }
+
+    // Populate the 'community' field
+
+    await user.populate([
+      {
+        path: "community",
+        model: Community,
+        select: "name id image _id",
+      },
+      {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    ]);
+    // await user.populate({
+    //     path: 'community',
+    //     model: Community,
+    //     select: 'name id image _id',
+    // }).execPopulate();
+
+    // // Populate the 'children' field and nested 'author' field
+    // await user.populate({
+    //     path: 'children',
+    //     model: Thread,
+    //     populate: {
+    //         path: 'author',
+    //         model: User,
+    //         select: 'name image id',
+    //     },
+    // }).execPopulate();
+
+    // The 'user' object now has populated 'community' and 'children' fields
+    return user as IUserDocument; // Casting to IUserDocument for correct return type
+  } catch (error) {
+    console.error("Error fetching user posts:", error);
+    // Handle the error or throw an exception based on your application's requirements
+    throw error;
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    await connectToDataBase();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+    const regex = new RegExp(searchString, "i");
+    const query: FilterQuery<typeof User> = {
+      id: {
+        $ne: userId,
+      },
+    };
+
+    if (searchString.trim() !== "") {
+      query.$or = [
+        {
+          username: {
+            $regex: regex,
+          },
+        },
+        {
+          name: {
+            $regex: regex,
+          },
+        },
+      ];
+    }
+
+    const sortOptions = {
+      createdAt: sortBy,
+    };
+
+    const userQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    // Count the total number of users that match the search criteria (without pagination).
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await userQuery.exec();
+
+    // Check if there are more users beyond the current page.
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    // throw error;
+
+    // You can log additional information or customize error messages based on the specific error
+    if (error instanceof Error) {
+      throw new Error(`Failed to fetch users: ${error.message}`);
+    } else {
+      throw new Error("Failed to fetch users. Unknown error.");
+    }
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    await connectToDataBase();
+    const userThreads = await Thread.find({
+      author: userId,
+    });
+
+    const childThreadIds = userThreads.reduce(
+      async (accPromise, userThread) => {
+        const acc = await accPromise;
+        return acc.concat(userThread.children);
+      },
+      Promise.resolve([])
+    );
+
+    const replies = await Thread.find({
+      _id: { $in: await childThreadIds },
+      author: {
+        $ne: userId,
+      },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
+    throw error;
+  }
 }
--- lib/dbConnection.ts
+++ lib/dbConnection.ts
@@ -1,20 +1,55 @@
+import mongoose, { ConnectOptions } from 'mongoose';
 import { log } from 'console';
-import {mongoose} from 'mongoose'
 
+let isConnectedToDB = false;
 
 export async function connectToDataBase() {
+    if (isConnectedToDB) {
+        log("Mongo DB Connection already established");
+        return;
+    }
 
     try {
-        const res = await mongoose.connect("mongodb://localhost:27017");
-        console.log(res);
-        
-    }
-    catch(e) {
-log(e);
+        await mongoose.connect("mongodb://localhost:27017", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        } as ConnectOptions);
 
+        // Listen for the connected event
+        mongoose.connection.once('connected', () => {
+            isConnectedToDB = true;
+            log("Mongo DB Connected");
+        });
+
+        // Listen for the error event
+        mongoose.connection.on('error', (err) => {
+            log(`Mongo DB Connection Error: ${err}`);
+            // Optionally, you might want to throw an error here if it's a critical connection error
+        });
+
+        // Listen for the disconnected event
+        mongoose.connection.on('disconnected', () => {
+            isConnectedToDB = false;
+            log("Mongo DB Disconnected");
+        });
+
+        // Listen for the SIGINT event (Ctrl+C in the console)
+        process.on('SIGINT', async () => {
+            await mongoose.connection.close();
+            process.exit(0);
+        });
+    } catch (error) {
+        log(`Error connecting to MongoDB: ${error}`);
+        
+        if (error instanceof mongoose.Error) {
+            // Mongoose-specific errors
+            if (error.name === 'ValidationError') {
+                log('Mongoose Validation Error:', error.message);
+                // Handle validation errors, possibly log or throw them
+            }
+        }
+
+        throw error; // Propagate the error to the caller
     }
 }
-
-
-
-
